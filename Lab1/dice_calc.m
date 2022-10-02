0e50d7c17808fcc36af00b7e9ccf8fee459d5599
marwan <marwankefah@gmail.com>
2022-10-02 16:59:06 +0200

    calculate dice score

--- Lab1/dice_calc.m
+++ Lab1/dice_calc.m
@@ -1,40 +1,102 @@
 %%addpath C:\Users\maraw\OneDrive\Documents\MATLAB\spm12\spm12
+D = 'data';
+S = dir(fullfile(D,'*'));
+N = setdiff({S([S.isdir]).name},{'.','..'});
+T1_GM_dice=[];
+T1_WM_dice=[];
+T1_CSF_dice=[];
+FLAIR_GM_dice=[];
+FLAIR_WM_dice=[];
+FLAIR_CSF_dice=[];
 
-GT=niftiread('data\1\LabelsForTesting.nii');
+for ii = 1:numel(N)
+    subfolder_name=fullfile(D,N{ii});
+    T1_nii_filename = fullfile(subfolder_name,'T1.nii'); % improve by specifying the file extension.
+    T2Falir_nii_filename = fullfile(subfolder_name,'T2_FLAIR.nii'); % improve by specifying the file extension.
+    GT=niftiread(fullfile(subfolder_name,'LabelsForTesting.nii'));
+    
+    class_GM_T1= double(niftiread(fullfile(subfolder_name,'c1T1.nii')))/double(255);
+    class_WM_T1= double(niftiread(fullfile(subfolder_name,'c2T1.nii')))/double(255);
+    class_CSF_T1= double(niftiread(fullfile(subfolder_name,'c3T1.nii')))/double(255);
+    class_B_T1= double(niftiread(fullfile(subfolder_name,'c4T1.nii')))/double(255);
+    class_ST_T1= double(niftiread(fullfile(subfolder_name,'c5T1.nii')))/double(255);
+    class_BG_T1= double(niftiread(fullfile(subfolder_name,'c6T1.nii')))/double(255);
+    
+    [mx_T1,pmx_T1]=max(cat(4,class_CSF_T1,class_GM_T1,class_WM_T1,class_B_T1,class_ST_T1,class_BG_T1),[],4);
 
-class_GM_T1= double(niftiread('data\1\c1T1.nii'))/double(255);
-class_WM_T1= double(niftiread('data\1\c2T1.nii'))/double(255);
-class_CSF_T1= double(niftiread('data\1\c3T1.nii'))/double(255);
-class_B_T1= double(niftiread('data\1\c4T1.nii'))/double(255);
-class_ST_T1= double(niftiread('data\1\c5T1.nii'))/double(255);
-class_BG_T1= double(niftiread('data\1\c6T1.nii'))/double(255);
+    class_GM_Flair= double(niftiread(fullfile(subfolder_name,'c1T2_FLAIR.nii')))/double(255);
+    class_WM_Flair= double(niftiread(fullfile(subfolder_name,'c2T2_FLAIR.nii')))/double(255);
+    class_CSF_Flair= double(niftiread(fullfile(subfolder_name,'c3T2_FLAIR.nii')))/double(255);    
+    class_B_Flair= double(niftiread(fullfile(subfolder_name,'c4T2_FLAIR.nii')))/double(255);
+    class_ST_Flair= double(niftiread(fullfile(subfolder_name,'c5T2_FLAIR.nii')))/double(255);
+    class_BG_Flair= double(niftiread(fullfile(subfolder_name,'c6T2_FLAIR.nii')))/double(255);
+    
+    [mx_flair,pmx_flair]=max(cat(4,class_CSF_Flair,class_GM_Flair,class_WM_Flair,class_B_Flair,class_ST_Flair,class_BG_Flair),[],4);
+
+    GT_CSF = (GT==1);
+    GT_GM = (GT==2);
+    GT_WM = (GT==3);
+
+    t1_CSF = (pmx_T1==1);
+    t1_GM = (pmx_T1==2);
+    t1_WM = (pmx_T1==3);
+    
+    flair_CSF = (pmx_flair==1);
+    flair_GM = (pmx_flair==2);
+    flair_WM = (pmx_flair==3);
+        
+    disp('GM T1:'); 
+    T1_GM_dice(end+1)=dice(t1_GM,GT_GM);
+    disp(T1_GM_dice(end));
+    disp('WM T1:');
+    T1_WM_dice(end+1)= dice(t1_WM,GT_WM);
+    disp(T1_WM_dice(end));
+    disp('CSF T1:');
+    T1_CSF_dice(end+1)= dice(t1_CSF,GT_CSF);
+    disp(T1_CSF_dice(end));
+    
+    disp('GM Flair:'); 
+    FLAIR_GM_dice(end+1)= dice(flair_GM,GT_GM);
+    disp(FLAIR_GM_dice(end));
+    disp('WM Flair:');
+    FLAIR_WM_dice(end+1)=dice(flair_WM,GT_WM);
+    disp(FLAIR_WM_dice(end));
+    disp('CSF Flair:');
+    FLAIR_CSF_dice(end+1)=dice(flair_CSF,GT_CSF);
+    disp(FLAIR_CSF_dice(end));
+
+mean(T1_GM_dice);
+std(T1_GM_dice);
+mean(T1_WM_dice);
+std(T1_WM_dice);
+mean(T1_CSF_dice);
+std(T1_CSF_dice);
+
+mean(FLAIR_GM_dice);
+std(FLAIR_GM_dice);
+mean(FLAIR_WM_dice);
+std(FLAIR_WM_dice);
+mean(FLAIR_CSF_dice)
+std(FLAIR_CSF_dice);
+end
 
 
-[mx,pmx]=max(cat(4,class_CSF_T1,class_GM_T1,class_WM_T1,class_B_T1,class_ST_T1,class_BG_T1),[],4);
 
-t1_CSF = (pmx==1);
-t1_GM = (pmx==2);
-t1_WM = (pmx==3);
-
-% class_GM_Flair= double(niftiread('data\1\c1T2_FLAIR.nii'))/double(255);
-% class_WM_Flair= double(niftiread('data\1\c2T2_FLAIR.nii'))/double(255);
-% class_CSF_Flair= double(niftiread('data\1\c3T2_FLAIR.nii'))/double(255);
-
-GT_CSF = (GT==1);
-GT_GM = (GT==2);
-GT_WM = (GT==3);
-
-disp('GM T1:'); 
-disp(dice(t1_GM,GT_GM));
-disp('WM T1:');
-disp(dice(t1_WM,GT_WM));
-disp('CSF T1:');
-disp(dice(t1_CSF,GT_CSF));
+%    disp('GM T1:'); 
+%     disp(dice((class_GM_T1>0.5),GT_GM));
+%     disp('WM T1:'); 
+%     disp(dice((class_WM_T1>0.5),GT_WM));
+%     disp('CSF T1:'); 
+%     disp(dice((class_CSF_T1>0.5),GT_CSF));
+%     
+%     disp('GM FLAIR:'); 
+%     disp(dice((class_GM_Flair>0.5),GT_GM));
+%     disp('WM FLAIR:'); 
+%     disp(dice((class_WM_Flair>0.5),GT_WM));
+%     disp('CSF FLAIR:'); 
+%     disp(dice((class_CSF_Flair>0.5),GT_CSF));
 
 
-% disp('GM FLAIR:'); 
-% disp(dice((class_GM_Flair>0.5),GT_GM));
-% disp('WM FLAIR:'); 
-% disp(dice((class_WM_Flair>0.5),GT_WM));
-% disp('CSF FLAIR:'); 
-% disp(dice((class_CSF_Flair>0.5),GT_CSF));
+
+
+
