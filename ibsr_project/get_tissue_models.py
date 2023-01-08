006daf1252fa9197ff8c3a51a930cecdcb2b1478
manasikattel <manasikattel1@gmail.com>
2023-01-08 20:18:18 +0100

    clean up tissue models script

--- ibsr_project/get_tissue_models.py
+++ ibsr_project/get_tissue_models.py
@@ -23,70 +23,7 @@ WMLabel = 3
 BGLabel = 0
 
 
-def visualize(image, label, filename):
-
-    filename.parent.mkdir(exist_ok=True, parents=True)
-    fig, ax = plt.subplots(1, 3)
-    print(np.unique(label))
-    ax[0].imshow(image[:, :, 128])
-    ax[1].imshow(label[:, :, 128])
-    ax[2].hist(image.flatten())
-
-    fig.savefig(filename.parent / Path(filename.stem + ".png"))
-
-
-def calc_prob_sum1(image, label, visualize=True):
-    # image *= 255.0 / image.max()
-    image = image.astype(int)
-
-    bg = image * (label == BGLabel)
-    csf = image * (label == CSFLabel)
-    gm = image * (label == GMLabel)
-    wm = image * (label == WMLabel)
-
-    # csf = csf[csf > 0]
-    # mask = label >= 1
-    image = image.flatten()
-    label = label.flatten()
-
-    # p_tissue_csf = sum(label == 1) / len(image)
-    # p_tissue_gm = sum(label == 3) / len(image)
-    # p_tissue_wm = sum(label == 2) / len(image)
-
-    onlybrain = image[label != 0]
-    mask = label[label != 0]
-    bg = image[label == 0]
-    csf = onlybrain[mask == CSFLabel]
-    gm = onlybrain[mask == GMLabel]
-    wm = onlybrain[mask == WMLabel]
-    intensity_hist = np.bincount(image, minlength=np.amax(onlybrain))
-
-    p_intensity_bg = np.bincount(bg, minlength=np.amax(image) + 1)
-    # / intensity_hist
-
-    p_intensity_csf = (
-        np.bincount(csf, minlength=np.amax(onlybrain) + 1) / intensity_hist
-    )
-    p_intensity_gm = np.bincount(gm, minlength=np.amax(onlybrain) + 1) / intensity_hist
-
-    # / intensity_hist
-    p_intensity_wm = np.bincount(wm, minlength=np.amax(onlybrain) + 1) / intensity_hist
-
-    # / intensity_hist
-    if visualize:
-        plt.plot(range(len(p_intensity_csf)), p_intensity_csf, alpha=0.5, label="csf")
-        plt.plot(range(len(p_intensity_gm)), p_intensity_gm, alpha=0.5, label="gm")
-        plt.plot(range(len(p_intensity_wm)), p_intensity_wm, alpha=0.5, label="wm")
-        plt.plot(range(len(p_intensity_bg)), p_intensity_bg, alpha=0.5, label="bg")
-
-        plt.legend()
-        plt.savefig(output_folder / Path("intensity_prob_bg.png"))
-        plt.show()
-
-    return p_intensity_wm, p_intensity_gm, p_intensity_csf
-
-
-def calc_prob(image, label):
+def calc_prob(image, label, plot=False):
 
     image = image.astype(int)
 
@@ -94,14 +31,9 @@ def calc_prob(image, label):
     gm = image * (label == GMLabel)
     wm = image * (label == WMLabel)
     bg = image * (label == BGLabel)
-    # csf = csf[csf > 0]
-    # mask = label >= 1
+
     image = image.flatten()
     label = label.flatten()
-
-    # p_tissue_csf = sum(label == 1) / len(image)
-    # p_tissue_gm = sum(label == 3) / len(image)
-    # p_tissue_wm = sum(label == 2) / len(image)
 
     onlybrain = image[label != 0]
     mask = label[label != 0]
@@ -115,19 +47,15 @@ def calc_prob(image, label):
     WM_hist, _ = np.histogram(wm, bins, density=True)
     bins = bins[:-1]
     # plot histograms
-    plt.plot(bins, CSF_hist, bins, GM_hist, bins, WM_hist)
-    # plt.title("CSF Histogram")
-    plt.xlabel("Intensity")
-    plt.ylabel("Probability")
-    plt.legend(["CSF", "GM", "WM"])
-    plt.savefig(output_folder / Path("intensity_hist.png"))
-
-    plt.show()
+    if plot:
+        plt.plot(bins, CSF_hist, bins, GM_hist, bins, WM_hist)
+        # plt.title("CSF Histogram")
+        plt.xlabel("Intensity")
+        plt.ylabel("Probability")
+        plt.legend(["CSF", "GM", "WM"])
+        plt.savefig(output_folder / Path("intensity_hist.png"))
+        plt.show()
     return WM_hist, GM_hist, CSF_hist
-
-
-def model_args_fn(x):
-    return x[:, 0], x[:, 1:]
 
 
 def normalize(image, max_val):
@@ -142,16 +70,12 @@ def normalize(image, max_val):
 def main(image_files, label_files):
     images = [sitk.GetArrayFromImage(sitk.ReadImage(str(i))) for i in image_files]
     labels = [sitk.GetArrayFromImage(sitk.ReadImage(str(i))) for i in label_files]
-    # masks = [nib.load(i).get_fdata() for i in mask_files]
 
     skull_stripped_normalized = [normalize(i, 255) for i in images]
     p_intensity_wm, p_intensity_gm, p_intensity_csf = calc_prob(
         np.stack(skull_stripped_normalized), np.stack(labels)
     )
 
-    # p_intensity_wm, p_intensity_gm, p_intensity_csf = calc_prob_sum1(
-    #     np.stack(skull_stripped_normalized), np.stack(labels)
-    # )
     prob_df = pd.DataFrame(
         {
             "intensity": range(len(p_intensity_csf)),
